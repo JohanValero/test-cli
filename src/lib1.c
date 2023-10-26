a907b647aa2d9670f518a9ca011914a0575dc93f
JohanValero <Johan.Pool.1994@gmail.com>
2023-10-26 04:55:22 +0000

    lib1 modified

--- src/lib1.c
+++ src/lib1.c
@@ -2,10 +2,11 @@
 
 // Una función simple para imprimir un saludo
 void greet() {
-    printf("* Testing database connection.\n");
-    printf("* Connection is OK.\n");
+    printf("  Testing database connection.\n");
+    printf("  Test connection is OK.\n");
 }
 
 void test1_func() {
-    printf("* Executing lib1.test1_func().\n");
+    printf("  Executing lib1.test1_func().\n");
+    printf("  Test finished.\n");
 }
